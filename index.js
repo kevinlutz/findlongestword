3277047b5410c48d1646ce20ed0b7d425bbe2597
Kevin Lutz <kevinlutz@gmail.com>
2021-06-10 17:51:13 +0000

    Birthday Cake Candles

--- index.js
+++ index.js
@@ -101,4 +101,18 @@ function aVeryBigSum(ar) {
 
 
 //#9
+//Birthday Cake Candles
+//You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+function birthdayCakeCandles(candles) {
+
+    const max = Math.max(...candles)
+    let sum = 0
+
+    for(const item of candles){
+        if(item === max) sum += 1
+    }
+    return sum
+}
+
+//#10
 //
