2114c8b0c034356708aa04a192388a813f3f8ab2
Kevin Lutz <kevinlutz@gmail.com>
2021-06-10 14:28:32 +0000

    celsius to fahrenheit

--- index.js
+++ index.js
@@ -55,3 +55,15 @@ function chunkArrayInGroups(arr, size) {
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
+//#5
+//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+function convertToF(celsius) {
+  let fahrenheit = celsius * (9/5) + 32
+  return fahrenheit;
+}
+
+convertToF(30);
+
+
+//#6
