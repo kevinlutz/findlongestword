09ab967ae1770d2e43c697c7a0c415b698cfd2b3
Kevin Lutz <kevinlutz@gmail.com>
2021-06-10 01:48:59 +0000

    new algo AM/PM to Military Time

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ function findLongestWord(str) {
 }
 
 findLongestWord('The dog ate my homework')
+
+
+
+//Change AM/PM to Military Time:
+function timeConversion(s) {
+    const newString = s.substring(0, s.length-2);
+    const array = newString.split(':');
+    const hour = array[0];
+    const minutes = array[1];
+    const seconds = array[2];
+    const hourNumber = parseInt(hour);
+    if (hourNumber === 12 && s.includes('PM')) {
+        return `${hour}:${minutes}:${seconds}`
+    } else if (s.includes('PM')){
+        const newHour = hourNumber + 12
+        return `${newHour}:${minutes}:${seconds}`
+    } else if (hourNumber === 12 && s.includes('AM')) { 
+        return `00:${minutes}:${seconds}`
+    } else {
+        return `${hour}:${minutes}:${seconds}`
+    }
+}
