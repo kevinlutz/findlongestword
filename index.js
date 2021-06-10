6085840719d29f096b6a0fddb9b6d2eb73dd89d2
Kevin Lutz <kevinlutz@gmail.com>
2021-06-10 14:26:40 +0000

    added algo called Chunky Monkey

--- index.js
+++ index.js
@@ -41,3 +41,17 @@ function reverseString(str) {
 
 reverseString("hello");
 
+
+//#4
+//Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+function chunkArrayInGroups(arr, size) {
+  let newArray = []
+  while (arr.length > 0) {
+      newArray.push(arr.splice(0, size));
+  }
+  return newArray;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
